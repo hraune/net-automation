8524b1dad18d9e5a9918f61cb4e429cb9390cb45
hraune <10920868+hraune@users.noreply.github.com>
2018-10-29 22:03:06 +0100

    updated balanced_graph abomination

--- reporting/filter_plugins/filters.py
+++ reporting/filter_plugins/filters.py
@@ -13,21 +13,39 @@ def timestamper(arg):
     return str('{:%Y%m%d-%H%M%S}'.format(datetime.datetime.now()))+'_'+arg
 
 
-def balance_graph(arg):
+def balance_graph(graph):
     """ Takes a list of neighbor tuples and returns it 
     reverse-sorted based on neighbor occurences  """
     
-    #Counts how many times each neighbor occurs
-    count = collections.Counter([x for (x,y) in arg]+[y for (x,y) in arg])
-    
-    #Expand each neighbor tuple with a summary of its neighbor occurence counts    
-    temp = [tup + (count[tup[0]]+count[tup[1]],) for tup in arg]
+    # Counts how many times each neighbor occurs
+    count = collections.Counter([x for (x,y) in graph]+[y for (x,y) in graph])
 
-    #Reverse sort neighbor tuples based on neighbor count
-    temp.sort(key=lambda item: item[2], reverse=True)
+    # Group neighbors based on neighbor occurence count
+    neigh_count = {}
+    for tup in graph:
+        total = count[tup[0]]+count[tup[1]]
+        if count[total] in neigh_count:
+            neigh_count[total].append(tup)
+        else:
+            neigh_count[total] = [tup]
 
-    #Return list of neighbor tuples without neighbor count
-    return [(x,y) for (x,y,z) in temp]
+    # Within tuples with same neighbor count, reorder tuples so 
+    # that the most occuring neighbor is first
+    reordered = []
+    for key in neigh_count:
+        value = neigh_count[key]
+        count = collections.Counter([x for (x,y) in value]+[y for (x,y) in value])
+        for tup in value:
+            if count[tup[1]] > count[tup[0]]:
+                reordered.append((tup[1],tup[0],key))
+            else:
+                reordered.append(tup+(key,))
+
+    # Reverse sort neighbor tuples based on neighbor count
+    reordered.sort(key=lambda item: item[2], reverse=True)
+
+    # Return list of neighbor tuples without neighbor count
+    return [(x,y) for (x,y,z) in reordered]
 
 
 class FilterModule(object): 
