676d36b19a95c07fe197f2a314b7e41c50909513
hraune <10920868+hraune@users.noreply.github.com>
2018-10-29 17:08:50 +0100

    Added balance_graph filter to filters.py

--- reporting/filter_plugins/filters.py
+++ reporting/filter_plugins/filters.py
@@ -1,13 +1,33 @@
 import datetime
+import collections
 
 def uppercase_all(arg): 
     return arg.upper() 
 
+
 def split_newlines(arg,index):
-	return arg.split('\n')[index]
+    return arg.split('\n')[index]
+
 
 def timestamper(arg):
-	return str('{:%Y%m%d-%H%M%S}'.format(datetime.datetime.now()))+'_'+arg
+    return str('{:%Y%m%d-%H%M%S}'.format(datetime.datetime.now()))+'_'+arg
+
+
+def balance_graph(arg):
+    """ Takes a list of neighbor tuples and returns it 
+    reverse-sorted based on neighbor occurences  """
+    
+    #Counts how many times each neighbor occurs
+    count = collections.Counter([x for (x,y) in arg]+[y for (x,y) in arg])
+    
+    #Expand each neighbor tuple with a summary of its neighbor occurence counts    
+    temp = [tup + (count[tup[0]]+count[tup[1]],) for tup in arg]
+
+    #Reverse sort neighbor tuples based on neighbor count
+    temp.sort(key=lambda item: item[2], reverse=True)
+
+    #Return list of neighbor tuples without neighbor count
+    return [(x,y) for (x,y,z) in temp]
 
 
 class FilterModule(object): 
@@ -15,5 +35,6 @@ class FilterModule(object):
         return {
         'uppercase_all': uppercase_all,
         'split_newlines': split_newlines,
-        'timestamper': timestamper
-        } 
+        'timestamper': timestamper,
+        'balance_graph': balance_graph,
+        }
