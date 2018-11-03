b923aa87264ae4a10e3c8eace2414d9a5ddb7994
hraune <10920868+hraune@users.noreply.github.com>
2018-11-03 19:39:42 +0100

    added remaining linknet calculation

--- L3VPN/filter_plugins/filters.py
+++ L3VPN/filter_plugins/filters.py
@@ -1,4 +1,5 @@
 # filters
+from netaddr import IPSet
 
 def uppercase_all(arg): 
     return arg.upper() 
@@ -7,6 +8,15 @@ def uppercase_all(arg):
 def split_newlines(arg,index):
     return arg.split('\n')[index]
 
+
+def subtract_subnet(original_subnet, remove_subnets):
+    """Remove a list of subnets from original subnet and returns a
+    list of new subnets sans those removed"""
+    
+    orinal = IPSet(original_subnet)
+    for subnet in remove_subnets:
+        orinal.remove(subnet)
+    return [str(subnet) for subnet in orinal.iter_cidrs()]
 
 class FilterModule(object): 
     def filters(self): 
