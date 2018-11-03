b5144ace0942747311e9ad4c7130abb5383d67ac
hraune <10920868+hraune@users.noreply.github.com>
2018-11-03 21:15:19 +0100

    added remaining linknet calculation

--- L3VPN/filter_plugins/filters.py
+++ L3VPN/filter_plugins/filters.py
@@ -2,9 +2,8 @@
 from netaddr import IPSet
 
 def subtract_subnet(original_subnet, remove_subnets):
-    """Remove a list of subnets from original subnet and returns a
-    list of new subnets sans those removed"""
-    print(original_subnet, remove_subnets)
+    """Subtract a list of subnets from original subnet and return leftover
+    subnet(s) in a list"""
     orinal = IPSet([original_subnet])
     for subnet in remove_subnets:
         orinal.remove(subnet)
