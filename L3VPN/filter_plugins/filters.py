442ee02b258d6c1cf22a2804553986a6b7769eff
hraune <10920868+hraune@users.noreply.github.com>
2018-11-06 22:06:56 +0100

    tweaking node generation

--- L3VPN/filter_plugins/filters.py
+++ L3VPN/filter_plugins/filters.py
@@ -9,7 +9,7 @@ def subtract_subnet(original_subnet, remove_subnets):
         orinal.remove(subnet)
     return [str(subnet) for subnet in orinal.iter_cidrs()]
 
-def in_list(tuplist, value):
+def is_in_tuplist(tuplist, value):
     """Checks if value is present in a list of tuples"""
     return len([tup for tup in tuplist if value in tup]) > 0
 
