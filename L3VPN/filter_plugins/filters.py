ace262e4d36193c232c05050fd0d417e5e4e3aa0
hraune <10920868+hraune@users.noreply.github.com>
2018-11-12 22:15:15 +0100

    fixed some spelling errors

--- L3VPN/filter_plugins/filters.py
+++ L3VPN/filter_plugins/filters.py
@@ -4,10 +4,10 @@ from netaddr import IPSet
 def subtract_subnet(original_subnet, remove_subnets):
     """Subtract a list of subnets from original subnet and return leftover
     subnet(s) in a list"""
-    orinal = IPSet([original_subnet])
+    original = IPSet([original_subnet])
     for subnet in remove_subnets:
-        orinal.remove(subnet)
-    return [str(subnet) for subnet in orinal.iter_cidrs()]
+        original.remove(subnet)
+    return [str(subnet) for subnet in original.iter_cidrs()]
 
 def is_in_tuplist(tuplist, value):
     """Checks if value is present in a list of tuples"""
