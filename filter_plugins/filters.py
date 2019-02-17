6af7882452345e3c70bbf7577dfabdf61b95af9e
hraune <10920868+hraune@users.noreply.github.com>
2019-02-17 16:53:18 +0100

    adding input validation tests

--- filter_plugins/filters.py
+++ filter_plugins/filters.py
@@ -1,10 +1,11 @@
 import datetime
+import collections
 from netaddr import IPSet
 
 def uppercase_all(arg): 
     return arg.upper() 
 
-def split_newlines(arg,index):
+def split_newlines(arg, index):
     return arg.split('\n')[index]
 
 def timestamper(arg):
@@ -18,6 +19,40 @@ def get_key(arg, i=0):
 
 def get_keys(arg):
     return list(arg.keys())
+
+def balance_graph(graph):
+    """ Takes a list of neighbor tuples and returns it 
+    reverse-sorted based on neighbor occurences  """
+    
+    # Counts how many times each neighbor occurs
+    count = collections.Counter([x for (x,y) in graph]+[y for (x,y) in graph])
+
+    # Group neighbors based on neighbor occurence count
+    neigh_count = {}
+    for tup in graph:
+        total = count[tup[0]]+count[tup[1]]
+        if total in neigh_count:
+            neigh_count[total].append(tup)
+        else:
+            neigh_count[total] = [tup]
+
+    # Within tuples with same neighbor count, reorder tuples so 
+    # that the most occuring neighbor is first
+    reordered = []
+    for key in neigh_count:
+        value = neigh_count[key]
+        count = collections.Counter([x for (x,y) in value]+[y for (x,y) in value])
+        for tup in value:
+            if count[tup[1]] > count[tup[0]]:
+                reordered.append((tup[1],tup[0],key))
+            else:
+                reordered.append(tup+(key,))
+
+    # Reverse sort neighbor tuples based on neighbor count
+    reordered.sort(key=lambda item: item[2], reverse=True)
+
+    # Return list of neighbor tuples without neighbor count
+    return [(x,y) for (x,y,z) in reordered]
 
 def subtract_subnet(original_subnet, remove_subnets):
     """Subtract a list of subnets from original subnet and return leftover
@@ -36,11 +71,16 @@ def nodes_not_in_fabric(nodes, provider_fabric, customer_fabric):
     """Returns nodes that are not defined in the fabric"""
     fabric = provider_fabric + customer_fabric
 
-    #Unpacks the fabric and node dictionaries, keeping only the names
+    # Unpacks the fabric and node dictionaries, keeping only the names
     nodes = [n['name'] for n in nodes]
     fabric = [node for f in fabric for node in [f['right'],f['left']]]
     
+    # Returns the list of fabric names subtracted from the list of all nodes
     return list(set(nodes) - set(fabric))
+
+def unique_list(arg):
+    '''Returns a list containing only unique elements, order is not preserved!'''
+    return list(set(arg))
 
 class FilterModule(object): 
     def filters(self): 
@@ -50,8 +90,10 @@ class FilterModule(object):
         'timestamper': timestamper,
         'format_uptime': format_uptime,
         'get_keys': get_keys,
+        'balance_graph':balance_graph,
         'get_key': get_key,
         'subtract_subnet': subtract_subnet,
         'is_in_tuplist': is_in_tuplist,
         'nodes_not_in_fabric': nodes_not_in_fabric,
         } 
+
