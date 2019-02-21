5ed740e4b4de46a1874908628324c98098e442f6
hraune <10920868+hraune@users.noreply.github.com>
2019-02-21 22:44:06 +0100

    adding template based validation

--- filter_plugins/filters.py
+++ filter_plugins/filters.py
@@ -1,23 +1,31 @@
 import datetime
 import collections
+from math import ceil
 from netaddr import IPSet
+import ipaddress
 
-def uppercase_all(arg): 
+def uppercase_all(arg):
+    '''Returns arg in upper case'''
     return arg.upper() 
 
 def split_newlines(arg, index):
+    '''Based on index, returns a given line of arg'''
     return arg.split('\n')[index]
 
 def timestamper(arg):
+    '''Returns a timestamp'''
     return str('{:%Y%m%d-%H%M%S}'.format(datetime.datetime.now()))+'_'+arg
 
 def format_uptime(arg):
+    '''Returns arg number of seconds as seconds, minutes, hours, days'''
     return str(datetime.timedelta(seconds=arg))
 
 def get_key(arg, i=0):
+    '''Returns a given key of a dict'''
     return list(arg.keys())[i]
 
 def get_keys(arg):
+    '''Returns all the keys of a dict'''
     return list(arg.keys())
 
 def balance_graph(graph):
@@ -25,7 +33,8 @@ def balance_graph(graph):
     reverse-sorted based on neighbor occurences  """
     
     # Counts how many times each neighbor occurs
-    count = collections.Counter([x for (x,y) in graph]+[y for (x,y) in graph])
+    count = collections.Counter([x for (x,y) in graph]
+                                    + [y for (x,y) in graph])
 
     # Group neighbors based on neighbor occurence count
     neigh_count = {}
@@ -41,7 +50,8 @@ def balance_graph(graph):
     reordered = []
     for key in neigh_count:
         value = neigh_count[key]
-        count = collections.Counter([x for (x,y) in value]+[y for (x,y) in value])
+        count = collections.Counter([x for (x,y) in value]
+                                        + [y for (x,y) in value])
         for tup in value:
             if count[tup[1]] > count[tup[0]]:
                 reordered.append((tup[1],tup[0],key))
@@ -79,7 +89,8 @@ def nodes_not_in_fabric(nodes, provider_fabric, customer_fabric):
     return list(set(nodes) - set(fabric))
 
 def unique_list(arg):
-    '''Returns a list containing only unique elements, order is not preserved!'''
+    '''Returns a list containing only unique elements, order is 
+    not preserved!'''
     return list(set(arg))
 
 def fabric_dict_to_tuples(fab):
@@ -123,6 +134,8 @@ def nodes_is_fully_defined(arg):
     return True
 
 def bgp_is_fully_defined(arg):
+    '''Verifies that all bgp entries are valid; the AS Number is valid and that
+    each neighbor pair contains 2 routers'''
     for bgp in arg:
         if not ('asn' in bgp and 1 < bgp['asn'] < 4294967294):
             return False
@@ -134,6 +147,10 @@ def bgp_is_fully_defined(arg):
     return True
 
 def hardware_is_fully_defined(arg):
+    '''Verifies that all hardware entries are valid; name is present, a 
+    management address is defined and is using an interface present in the 
+    hardware, a list of all possible interfaces is defined and that there is 
+    devices assigned to the hardware.'''
     for hardware in arg:
         if not ('name' in hardware and hardware['name'] != ''):
             return False
@@ -147,6 +164,71 @@ def hardware_is_fully_defined(arg):
             return False
     return True
 
+def fabric_is_fully_defined(arg, hw):
+    '''Verifies that a fabric is valid; left node and its left_port is defined,
+    right node and its right_port is defined and that all ports are interfaces
+    defined in the hardware.'''
+    all_intfs = [intf for intf_list in hw for intf in intf_list['interfaces']]
+    for fabric in arg:
+        if not ('left' in fabric and fabric['left'] != ''):
+            return False
+        if not ('left_port' in fabric and fabric['left_port'] != '' 
+                    and fabric['left_port'] in all_intfs):
+            return False
+        if not ('right' in fabric and fabric['right'] != ''):
+            return False                    
+        if not ('right_port' in fabric and fabric['right_port'] != '' 
+                    and fabric['right_port'] in all_intfs):
+            return False
+    return True
+
+def proper_linknets(arg):
+    '''Verifies that a list of linknets are all proper IP network addresses'''
+    for linknet in arg:
+        try:
+            network_addr = ipaddress.ip_network(linknet)
+        except ValueError:
+            return False
+    return True
+
+def proper_ip_address(arg):
+    '''Verifies that a given IP is a proper IP address'''
+    try:
+        ip_addr = ipaddress.ip_address(arg)
+    except ValueError:
+        return False
+    return True
+
+def check_route_distinguisher(arg):
+    '''Checks if a given arg is a valid route distinguisher(RD). A valid RD is
+    6 bytes; (2byte ASN):(4byte any value) or (4byte IP):(2byte any value)'''
+    if ':' in arg and len(arg.split(':')) == 2:
+        front,back = arg.split(':')
+        if proper_ip_address(front) and ceil(int(back).bit_length()/8.0) <= 2:
+            return True
+        elif (not proper_ip_address(front) 
+                and ceil(int(front).bit_length()/8) <= 2 
+                and ceil(int(back).bit_length()/8) <= 4):
+            return True
+        else:
+            return False
+    return False
+    
+def vpnv4_is_fully_defined(arg):
+    '''Verifies that a list vpnv4 services is valid; valid route distinguisher,
+    ospf ID, list of nodes and a state'''
+    for vpn in arg:
+        if not  (vpn['name'] != '' and len(vpn['name'] <= 32)):
+            return False
+        if not (check_route_distinguisher(vpn['rd'])):
+            return False
+        if not (1 <= vpn['ospfid'] <= 65535):
+            return False
+        if not (isinstance(vpn['nodes'], dict) and len(vpn['nodes']) >= 2):
+            return False
+        if vpn['state'] == '':
+            return False
+    return True
 
 class FilterModule(object): 
     def filters(self): 
@@ -167,9 +249,14 @@ class FilterModule(object):
         'get_all_fabric_names':get_all_fabric_names,
         'subtract_list':subtract_list,
         'merge_lists_from_list_of_dict':merge_lists_from_list_of_dict,
-        'count_unique_entries_in_list_of_dicts':count_unique_entries_in_list_of_dicts,
+        'unique_entries_in_list_of_dicts':unique_entries_in_list_of_dicts,
         'nodes_is_fully_defined':nodes_is_fully_defined,
         'bgp_is_fully_defined':bgp_is_fully_defined,
         'hardware_is_fully_defined':hardware_is_fully_defined,
+        'fabric_is_fully_defined':fabric_is_fully_defined,
+        'proper_linknets':proper_linknets,
+        'proper_ip_address':proper_ip_address,
+        'check_route_distinguisher':check_route_distinguisher,
+        'vpnv4_is_fully_defined':vpnv4_is_fully_defined,
         } 
 
